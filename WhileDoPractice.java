0c94c95f47f7b785d2509a6a9c499790602388e9
ꜱʜᴜʙʜᴀ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-05-15 18:41:58 -0400

    Add files via upload

---
+++ WhileDoPractice.java
@@ -0,0 +1,22 @@
+class WhileDoPractice{
+	public static void main(String[] args){
+	do{
+	System.out.println("Good Morning!");
+	break;
+	}
+	while (5<2);
+
+	System.out.println("***********************************");
+
+	do{
+	System.out.println("Good Evening!");
+	}
+	while (5<2);
+
+    //infinite loop
+	do{
+	System.out.println("Good Night!");
+	}
+	while (5>2);
+	}
+}
